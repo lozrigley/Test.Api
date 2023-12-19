02361cd51a6785fe38289c7693bedca3e9f47a37
Larry Rigley <larryrigley@gmail.com>
2023-12-19 20:29:54 +0000

    Added Order and included referential data I hope

--- src/Test.DataAccess/TestApiDbContext.cs
+++ src/Test.DataAccess/TestApiDbContext.cs
@@ -13,6 +13,21 @@ public class TestApiDbContext : DbContext
 
     public TestApiDbContext(DbContextOptions options) : base(options)
     {
+
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Product) // Each Order has one Product
+            .WithMany(p => p.Orders) // Product can have many Orders
+            .HasForeignKey(o => o.ProductId); // Foreign key is ProductId
         
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Customer) // Each Order has one Customer
+            .WithMany(c => c.Orders) // Customer can have many Orders
+            .HasForeignKey(o => o.CustomerId); // Foreign key is CustomerId
     }
 }
