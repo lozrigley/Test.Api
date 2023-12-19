2416c74750e0b6e953690d5cac14f5c7de4b3c12
Larry Rigley <larryrigley@gmail.com>
2023-12-19 18:41:13 +0000

    Another failing test. This time for PUT customer

--- test/Test.FunctionalTests/Api/CustomerTests.cs
+++ test/Test.FunctionalTests/Api/CustomerTests.cs
@@ -146,4 +146,49 @@ public class CustomerTests
         //Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
+
+    [Fact]
+    public async Task PutCustomerUpdatesCustomer()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactoryWithInMemoryDatabase();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        using (var scope = factory.Services.CreateScope())
+        {
+
+            var context = scope.ServiceProvider.GetRequiredService<TestApiDbContext>();
+            context.Database.EnsureCreated();
+            var customer = new Customer
+            {
+                Id = guid,
+                FirstName = "Test",
+                LastName = "Customer",
+                Email = "kangaroo@bounce.com",
+                Phone = "1234567890"
+            };
+            context.Customers.Add(customer);
+            await context.SaveChangesAsync();
+
+        }
+
+        var customerToUpdate = new
+        {
+            FirstName = "Test",
+            LastName = "Customer",
+            Email = "bird@swim.com"
+        };
+
+        //Act
+        var response = await client.PutAsJsonAsync($"/customers/{guid}", customerToUpdate);
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        var jsonDocument = await response.Content.ReadFromJsonAsync<JsonDocument>();
+        jsonDocument!.RootElement.GetProperty("id").GetGuid().Should().Be(guid);
+        jsonDocument!.RootElement.GetProperty("firstName").GetString().Should().Be("Test");
+        jsonDocument!.RootElement.GetProperty("lastName").GetString().Should().Be("Customer");
+        jsonDocument!.RootElement.GetProperty("email").GetString().Should().Be("bird@swim.com");
+        jsonDocument!.RootElement.GetProperty("phone").GetString().Should().Be("");
+    }
 }
