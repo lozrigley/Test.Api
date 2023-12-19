7921bbf48a5a01fdb1060625e214928aaf734635
Larry Rigley <larryrigley@gmail.com>
2023-12-19 22:40:18 +0000

    Almost finished

--- test/Test.FunctionalTests/Api/OrderTests.cs
+++ test/Test.FunctionalTests/Api/OrderTests.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
@@ -151,6 +152,327 @@ public class OrderTests
         
         //Act
         var response = await client.GetAsync($"/orders/{guid}");
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task CreateOrderSuccesfully()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactoryWithInMemoryDatabase();
+        var client = factory.CreateClient();
+        var product1 = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Product",
+            Description = "OrderTest",
+            SKU = "ABC1"
+        };
+        var customer1 = new Customer
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "Test",
+            LastName = "OrderTest",
+            Email = "asds",
+            Phone = "1234567890"
+        };
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<TestApiDbContext>();
+            context.Products.Add(product1);
+            context.Customers.Add(customer1);
+            context.Database.EnsureCreated();
+            await context.SaveChangesAsync();
+        }
+
+        var orderToCreate = new
+        {
+            CustomerId = customer1.Id,
+            ProductId = product1.Id,
+            Status = OrderStatus.New
+        };
+
+        //Act
+        var response = await client.PostAsJsonAsync("/orders", orderToCreate);
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        var jsonDocument = await response.Content.ReadFromJsonAsync<JsonDocument>();
+        jsonDocument!.RootElement.GetProperty("customerId").GetGuid().Should().Be(customer1.Id);
+        jsonDocument!.RootElement.GetProperty("productId").GetGuid().Should().Be(product1.Id);
+        jsonDocument!.RootElement.GetProperty("status").GetString().Should().Be("New");
+    }
+    
+    [Fact]
+    public async Task CreateOrderReturnsBadRequestWhenRequiredFieldsAreNotPopulated()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var orderToCreate = new
+        {
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PostAsJsonAsync("/orders", orderToCreate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task CreateOrderReturnsBadRequestWhenCustomerDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var orderToCreate = new
+        {
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PostAsJsonAsync("/orders", orderToCreate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task CreateOrderReturnsBadRequestWhenProductDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var orderToCreate = new
+        {
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PostAsJsonAsync("/orders", orderToCreate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task UpdateOrderSuccesfully()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var product1 = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Product",
+            Description = "OrderTest",
+            SKU = "ABC1"
+        };
+        var customer1 = new Customer
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "Test",
+            LastName = "OrderTest",
+            Email = "asds",
+            Phone = "1234567890"
+        };
+        var order1 = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer1.Id,
+            ProductId = product1.Id,
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+            Status = OrderStatus.New
+        };
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<TestApiDbContext>();
+            context.Products.Add(product1);
+            context.Customers.Add(customer1);
+            context.Orders.Add(order1);
+            context.Database.EnsureCreated();
+            await context.SaveChangesAsync();
+        }
+
+        var orderToUpdate = new
+        {
+            Id = order1.Id,
+            CustomerId = customer1.Id,
+            ProductId = product1.Id,
+            Status = OrderStatus.Delivered
+        };
+
+        //Act
+        var response = await client.PutAsJsonAsync($"/orders/{order1.Id}", orderToUpdate);
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        var jsonDocument = await response.Content.ReadFromJsonAsync<JsonDocument>();
+        jsonDocument!.RootElement.GetProperty("id").GetGuid().Should().Be(order1.Id);
+        jsonDocument!.RootElement.GetProperty("customerId").GetGuid().Should().Be(customer1.Id);
+        jsonDocument!.RootElement.GetProperty("productId").GetGuid().Should().Be(product1.Id);
+        jsonDocument!.RootElement.GetProperty("status").GetString().Should().Be("Delivered");
+    }
+    
+    [Fact]
+    public async Task PutOrderReturnsNotFoundWhenOrderDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var orderToUpdate = new
+        {
+            Id = guid,
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync($"/orders/{guid}", orderToUpdate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task PutOrderReturnsBadRequestWhenRequiredFieldsAreNotPopulated()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var orderToUpdate = new
+        {
+            Id = guid,
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync($"/orders/{guid}", orderToUpdate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task PutOrderReturnsBadRequestWhenCustomerDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var orderToUpdate = new
+        {
+            Id = guid,
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync($"/orders/{guid}", orderToUpdate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task PutOrderReturnsBadRequestWhenProductDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var orderToUpdate = new
+        {
+            Id = guid,
+            CustomerId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            Status = OrderStatus.New
+        };
+        
+        //Act
+        var response = await client.PutAsJsonAsync($"/orders/{guid}", orderToUpdate);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task DeleteOrderDeletesOrder()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactoryWithInMemoryDatabase();
+        var client = factory.CreateClient();
+        var product1 = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Product",
+            Description = "OrderTest",
+            SKU = "ABC1"
+        };
+        var customer1 = new Customer
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "Test",
+            LastName = "OrderTest",
+            Email = "asds",
+            Phone = "1234567890"
+        };
+        var order1 = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer1.Id,
+            ProductId = product1.Id,
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+            Status = OrderStatus.New
+        };
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<TestApiDbContext>();
+            context.Products.Add(product1);
+            context.Customers.Add(customer1);
+            context.Orders.Add(order1);
+            context.Database.EnsureCreated();
+            await context.SaveChangesAsync();
+        }
+
+        //Act
+        var response = await client.DeleteAsync($"/orders/{order1.Id}");
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        var response2 = await client.GetAsync($"/orders/{order1.Id}");
+        response2.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+    
+        
+    [Fact]
+    public async Task DeleteOrderReturnsNotFoundWhenOrderDoesNotExist()
+    {
+        //Arrange
+        var factory = new ApiWebApplicationFactory();
+        var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        
+        //Act
+        var response = await client.DeleteAsync($"/orders/{guid}");
         
         //Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
