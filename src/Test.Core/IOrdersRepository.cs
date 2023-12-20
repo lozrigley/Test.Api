02674f82915c65f72bf32232fa3f07f9df868e23
Larry Rigley <larryrigley@gmail.com>
2023-12-20 00:11:43 +0000

    Fixed ReadMe with instructions on how to run dockerfile and also how to use docker image of sql

--- src/Test.Core/IOrdersRepository.cs
+++ src/Test.Core/IOrdersRepository.cs
@@ -9,13 +9,21 @@ public interface IOrderRepository
     
     public Task<OneOf<Order, NotFound>> GetByIdAsync(Guid id);
     
-    public Task<OneOf<Order, MissingReferenceData>> CreateAsync(Order order);
+    public Task<OneOf<Order, MissingReferenceData>> CreateAsync(OrderDto orderDto);
     
-    public Task<OneOf<Order, NotFound, MissingReferenceData>> UpdateAsync(Order order);
+    public Task<OneOf<Order, NotFound, MissingReferenceData>> UpdateAsync(OrderDto orderDto);
     
     public Task<OneOf<Success,NotFound>> DeleteAsync(Guid id);
 }
 
 public record MissingReferenceData();
+
+public record OrderDto
+{
+    public Guid Id { get; init; }
+    public Guid CustomerId{ get; init; }
+    public Guid ProductId { get; init; }
+    public OrderStatus Status { get; init; }
+}
 
 
